2c0dc4afeb8be9d0ad487bf2fb4cef42b2cadd32
Quil <quyhoangtran0034@gmail.com>
2022-05-27 13:43:43 -0500

    change path.resolve to path.join

--- index.ts
+++ index.ts
@@ -201,7 +201,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 
 // // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
     
 
